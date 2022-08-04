70ba98ccba8554b9fb8e1962fd3fdbb18b9b854d
samtremblo <samtremblay.exe@gmail.com>
2022-08-04 13:44:15 -0400

    Update enableSMB.ps1

--- enableSMB.ps1
+++ enableSMB.ps1
@@ -9,17 +9,6 @@ $Name = 'requiresecuritySignature'
 $Value = '0'
 New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force 
 
-
-#Supposed to make local folders accessible without any protection, but windows 10 is shit so it doesnt work. Needs to be done manually so it wont block you from other computers. Remove folder protection in advanced network settings.
-$RegistryPath = 'HKLM:SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters'
-$Name = 'RestrictNullSessAccess'
-$Value = '0'
-New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force 
-$RegistryPath = 'HKLM:SYSTEM\CurrentControlSet\Control\Lsa'
-$Name = 'everyoneincludesanonymous'
-$Value = '1'
-New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force 
-
 #Automatically start smb
 Set-service -Name upnphost -StartupType Automatic
 Set-service -Name FDResPub -StartupType Automatic
